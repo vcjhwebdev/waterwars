5d2e90642ba5f3f9ceede8e51e168a492dc06d4e
Kyle Scheuerlein <kyle@kylesureline.com>
2019-01-23 14:41:39 -0800

    Use JS to add the class names
    Clicking on cells now logs their coordinate (ie. "A2")

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,28 @@
 var board = document.querySelector('table');
 var rows = board.querySelectorAll('tr');
 
+function addClassNamesToTable() {
+
+  function addClassNamesToCell(row) {
+    var letterCell = row.firstElementChild;
+    var letter = letterCell.textContent;
+    // start at second "column", since first column is the letter column
+    for(var i = 1; i < row.children.length; i++) {
+      var cell = row.children[i];
+      cell.className = letter + i;
+    }
+  }
+
+  // start at row 1 (letter A), not row 0
+  for(var i = 1; i < rows.length; i++) {
+    var row = rows[i];
+    addClassNamesToCell(row);
+  }
+}
+
+// Add class names to table
+addClassNamesToTable();
+
 board.addEventListener('click', function(e) {
   console.log(e.target.className);
 });
