328481325ab48b2cfd94a8e9534c24e58f2fc37e
Ashton Cho <ashton.cho@warriorlife.net>
2019-01-31 09:22:03 -0800

    made ships

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,9 @@
+var computerBoard = document.querySelector('#computer');
 var playerBoard = document.querySelector('#player');
-var rows = playerBoard.querySelectorAll('tr');
-var ting = prompt("What Do u want Ur symbol to BE Max 3 characters and More than 0");
+var rows = computerBoard.querySelectorAll('tr');
+var rows2 = computerBoard.querySelectorAll('tr');
+var ting = prompt("What Do u wa*nt Ur symbol to BE Max 3 characters and More than 0");
+var numOfShips = 5;
 
 function addClassNamesToTable() {
 
@@ -24,7 +27,29 @@ function addClassNamesToTable() {
 // Add class names to table
 addClassNamesToTable();
 
-playerBoard.addEventListener('click', function(e) {
+function addClassNamesToPlayerTable() {
+
+  function addClassNamesToCell(row2) {
+    var letterCell2 = row2.firstElementChild;
+    var letter2 = letterCell2.textContent;
+    // start at second "column", since first column is the letter column
+    for(var i = 1; i < row2.children.length; i++) {
+      var cell2 = row2.children[i];
+      cell2.className = letter2 + i;
+    }
+  }
+
+  // start at row 1 (letter A), not row 0
+  for(var i = 1; i < rows2.length; i++) {
+    var row2 = rows2[i];
+    addClassNamesToCell(row2);
+  }
+}
+
+// Add class names to table
+addClassNamesToPlayerTable();
+
+computerBoard.addEventListener('click', function(e) {
 
   if (e.target.innerHTML == " ")
   {
@@ -48,3 +73,15 @@ function limitter()
   }
 }
 limitter();
+
+playerBoard.addEventListener('click', function(e) {
+
+  if (e.target.innerHTML == " " && numOfShips >=1)
+  {
+    e.target.innerHTML = "Ship";
+    numOfShips -= 1;
+    e.target.id = "ship";
+
+  }
+console.log(e.target.id);
+});
