cea1a9ea36977bf822e26993aaf72802a12c83de
Ashton Cho <ashton.cho@warriorlife.net>
2019-03-20 09:49:11 -0700

    andded modal

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,4 @@
+
 var computerBoard = document.querySelector('#computer');
 var playerBoard = document.querySelector('#player');
 var rows = computerBoard.querySelectorAll('tr');
@@ -8,6 +9,7 @@ var boardThing = 100;
 var numOfShips = ships.length;
 var shipStart = false;
 var shipEnd = false;
+var ting = 0;
 var placements = [
   [
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -139,11 +141,28 @@ function hasPlacedAllShips() {
   }
 }
 
+function placeModal(content)
+{
+  var modal = document.createElement('div');
+  modal.className = 'modal';
+  var modalContent = `
+    <div class="modal-content">
+      <h1>${content}</h1>
+      <button type="button" onclick="reload()">Play Again</button>
+      <h1>STATISTICS</h1>
+      <h2>Moves taken: MOVES TAKEN</h2>
+    </div>`
+  modal.innerHTML = modalContent;
+  var body = document.querySelector('body');
+  body.insertBefore(modal, body.children[0]);  
+}
+
 
 // id="player-1-1"
 // id="player-1-2"
 
 function placeShips(e) {
+
 
   function seperate(target)
   {
@@ -212,62 +231,10 @@ function randomShips()
 {
   return Math.floor(Math.random() * 18)+1;
 }
-computerBoard.addEventListener('click', function(e) {
-boardThing -= 1;
-if(hasPlacedAllShips())
-{
-if(randomThing >= 1)
-{
-  randomShips();
-  if(randomShips() == 1)
-  {
-    if(e.target.innerHTML == " ")
-    {
-      e.target.innerHTML = "X";
-      randomThing -= 1;
-      e.target.className = "shot hit";
-    }
-  }
-  else
-  {
-    if(e.target.innerHTML == " ")
-    {
-    e.target.innerHTML = "O";
-    }
-  }
-}
-else if (randomThing <= 0)
-{
-  if(e.target.innerHTML == " ")
-  {
-  e.target.innerHTML = "O";
-  }
-  var alertThing = confirm("You win, do you want to try again?");
-  if (alertThing == true)
-  {
-    location.reload();
-  }
-  else {
-    location.reload();
-  }
-}
-}
-else {
-  alert("Please place your ships first.");
-}
-if(boardThing <= 5 && randomThing >=0)
-{
-    if(e.target.innerHTML == " ")
-    {
-      e.target.innerHTML = "X";
-      randomThing -= 1;
-      e.target.className = "shot hit";
-    }
-}
-});
 
 playerBoard.addEventListener('click', function(e) {
   placeShips(e);
+  ting +=1;
 });
 document.write("<h3 class='shipsLeft'>Ships Left: "+ numOfShips + "</h3>");
 document.write("<h3 class='shipLength'>Current Ship's Length: " + ships.pop() + "</h3>");
